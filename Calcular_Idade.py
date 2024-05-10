1acd5b16005403ebef210f053a560ac58f841bdc
Francisco Expedito Nascimento de Paulo <142537791+9313TaylDe@users.noreply.github.com>
2024-05-10 15:53:40 -0300

    Calcular Idade

---
+++ Calcular_Idade.py
@@ -0,0 +1,8 @@
+nome = input("Digite o seu bnome: ")
+idade = int(input("Digite a sua idade: "))
+
+if idade <= 17 or idade > 60:
+    print(f'{nome} você não é obrigado ou não pode votar!')
+
+elif idade >= 18 and idade < 60:
+    print(f"{nome}, o seu voto obrigaório!")
