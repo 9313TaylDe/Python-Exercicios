79f703e2ad01334de9759f8bbe776c94d4f8a820
Francisco Expedito Nascimento de Paulo <142537791+9313TaylDe@users.noreply.github.com>
2024-05-10 15:54:02 -0300

    Escolha opção

---
+++ Escolha_Opcao.py
@@ -0,0 +1,22 @@
+numero = int(input("Sua escolha, digite entre 1 e 6: "))
+
+if numero == 1:
+    print("Domingo")
+if numero == 2:
+    print("Escolheu o número 2")
+if numero == 3:
+    print("Escolheu o número 3")
+if numero == 4:
+    print("Escolheu o número 4")
+if numero == 5:
+    print("Escolheu o número 5")
+if numero == 6:
+    print("Escolheu o número 6")
+
+i = 0
+
+
+for i in range(i,10):
+    soma = 0
+    soma = i
+    print(f"A soma = {soma}")
