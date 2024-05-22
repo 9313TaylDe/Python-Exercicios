73bc1401a745ec5a5c1477f88ce5ce5562c1b139
me <me@gmail.comm>
2024-05-22 13:54:36 -0300

    Criando Grupos

---
+++ CriandoGrupos.py
@@ -0,0 +1,18 @@
+#Faça um algoritmo para cadastrar os alunos que estão se canditando para equipes de desenvolvimento
+
+#Monte 4 equipes de alunos
+
+
+equipe = [[],[],[],[]]
+
+# Lista de nomes de alunos
+
+nomes = set (['Caio','Davi', 'Felipe', 'Erick', 'Esmael', 'Alison', 'Expedito', 'Armando', 'Kauã', 'Marcos', 'Graziel', 'Raiane', 'Natanael', 'Gabriel', 'Pedro', 'Silas'])
+
+for j in range(4):
+  for i in range(4): 
+    equipe[i].append(nomes.pop())
+
+# Mostre os nomes por cada equipe
+for i in range(4): 
+  print(f'Equipe {i + 1}: {equipe[i]}\n')
