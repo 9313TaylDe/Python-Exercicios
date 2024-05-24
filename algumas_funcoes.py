24fdccf65f968cd64797d4014c67b6cb3d5d8bf9
me <me@gmail.comm>
2024-05-24 14:07:01 -0300

    Algumas funções

---
+++ algumas_funcoes.py
@@ -0,0 +1,33 @@
+texto = 'texto em minusculo'
+print(len(texto))
+
+print(texto.find('ex'))
+
+print(texto.upper()) #maiusculo
+
+print(texto.title()) #titulo
+
+print(texto.strip()) #remove espacos
+
+print(len(texto)) #tamanho da string
+
+print(texto.replace('texto','textos')) #troca strings
+
+print(texto.split()) #lista em substrings
+
+print(texto.isupper())
+
+print(texto.startswith('tex')) #comeca com tal palavra
+
+print(texto.endswith('lo'))
+
+print(texto.islower()) # Lletras minusculos
+
+print(texto.isalnum()) # e alfanumerico
+
+print(texto.isalpha()) #e numerico
+
+print(texto.isspace()) #contem espaços
+
+num = '1234'
+print(num.isdigit()) # e digitos
