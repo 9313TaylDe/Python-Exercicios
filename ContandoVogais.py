b8482d76e7aeb9a063288ef3fd736a9457a860eb
me <me@gmail.comm>
2024-05-24 15:46:25 -0300

    Contando Vogais

--- ContandoVogais.py
+++ ContandoVogais.py
@@ -1,11 +1,22 @@
 def ContaVogais():
     
     palavra = input("Digite uma palavra: ")
+    
     vogais = 'aeiouAEIOU'
+    numbers = 1,2,3,4,5,6,7,8,9
     consoantes = not 'aeiouAEIOU'
     somandoVogais = 0
+    espaco = 0
+    
     for letra in palavra:
         if letra in vogais:
             somandoVogais += 1
             print("Achei a quantidade de "+letra)
+        
+        elif letra == ' ':
+            espaco += 1
+            
+        else:
+                consoantes += 1
+            
 ContaVogais()
